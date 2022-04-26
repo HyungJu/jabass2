92913692bd5ddc10fed0f734f0fbfa3ff5197f17
hyunjin <leehj0110@kakao.com>
2022-04-26 23:51:15 +0900

    feat: login, signup markup

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ import { $ } from '../utils/index.js';
 import { PATHNAMES } from '../constants/pathnames.js';
 import { renderHome } from '../pages/home/view.js';
 import { renderHeader } from './header/view.js';
+import { renderSignup } from '../pages/signup/view.js';
 import { renderLogin } from '../pages/login/view.js';
 
 const $header = $('header');
@@ -9,11 +10,11 @@ const $main = $('main');
 
 const renderContents = {
   [PATHNAMES.HOME]: renderLogin,
-  [PATHNAMES.LOGIN]: renderHome,
+  [PATHNAMES.LOGIN]: renderLogin,
+  [PATHNAMES.SIGNUP]: renderSignup,
 };
 
 export const renderContent = (pathname) => {
-  console.log(pathname, 'what');
   renderContents[pathname]($main);
 };
 
